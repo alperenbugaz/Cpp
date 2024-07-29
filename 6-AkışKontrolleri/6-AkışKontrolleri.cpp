07bbff56629a5a1988205a6da14738b7d5d07ed9
alperenbugaz <alperenbugaz@gmail.com>
2024-07-30 02:22:13 +0300

    Akış Kontrolleri ve Diziler Konusu Eklendi

--- 6-AkışKontrolleri/6-AkışKontrolleri.cpp
+++ 6-AkışKontrolleri/6-AkışKontrolleri.cpp
@@ -2,6 +2,95 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+    // Conditional Statements --if, else switch case
+    //Jumps Goto --- Break, Continue
+    //Loops --- For, While, Do While
+    //Halts and Continue --- Return, Exit
+    //Exception Handling --- Try, Catch, Throw
+    int a = 10;
+    int b = 20;
+
+
+    // if statement
+    if (a > b)
+	{
+		std::cout << "a is greater than b\n";
+	}
+	else if (a < b)
+	{
+		std::cout << "b is greater than a\n";
+	}
+
+    else {
+        std::cout << "a is equal to b\n";
+    }
+
+    // switch case
+    int c =20;
+    char  d = 'a';
+
+    switch (d) {
+
+    case 10: std::cout << "c is 10\n"; break;
+
+    case 20: std::cout << "c is 20\n"; break;
+
+    case 30: std::cout << "c is 30\n"; break;
+
+    default: std::cout << "c is not 10, 20 or 30\n"; break; }
+    
+    //for loop
+    for (int i = 0; i < 10; i++)
+	{
+		std::cout << i << '\n';
+	}
+
+
+    //while loop
+    int j = 0;
+    while (true) {
+        j++;
+        std ::cout << j << '\n';
+        if (j == 10) {
+			std::cout << "j is 10 loop ended\n";
+            break;
+		}
+	}
+
+
+    //do while loop
+    int k = 0;
+    do {
+		k++;
+		std::cout << k << '\n';
+	} while (k < 10);
+    
+
+
+    //continue
+    for (int i = 10; i < 20; i++) {
+        printf("i is %d\n", i);
+        if (i == 12) {
+            std::cout << "5\n";
+            continue; 
+            std::cout << "6\n"; //this line will not be executed
+        }
+
+    }
+
+
+    for (int i = 10; i < 20; i++) {
+		printf("i is %d\n", i);
+		if (i == 12) {
+			goto stop;
+		}
+
+	}
+
+stop:
+    std::cout << "Program ended\n";
+
     return 0; 
 }
--- 7-Diziler/7-Diziler.cpp
+++ 7-Diziler/7-Diziler.cpp
@@ -1,20 +1,83 @@
-﻿// 7-Diziler.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
-//
-
-#include <iostream>
+﻿#include <iostream>
+void func1(int Array[2]);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int puan1 = 10;
+    int puan2 = 20;
+    int puan3 = 30;
+    int puan4 = 40;
+    int puanlar[4] = { puan1, puan2, puan3, puan4 }; 
+    int a = puanlar[0];
+    int puanlar2[2] = { 100,90 }; //static array
+
+    std::cout << a << '\n'; //10--puan1
+
+
+    int randomArray[5];
+    std::cout << randomArray[0] << '\n'; //random value
+
+    int Array1[5] = {}; //initialize all elements to 0
+    std::cout << Array1[0] << '\n'; //0
+    std::cout << Array1[1] << '\n'; //0
+    std::cout << Array1[2] << '\n'; //0
+    std::cout << Array1[3] << '\n'; //0
+    std::cout << Array1[4] << '\n'; //0
+
+
+    int Array2[2] = { 1,2 };
+
+    std::cout << Array2 << '\n'; //Adress
+    std::cout << *Array2 << '\n'; //1 
+    std::cout << *Array2 + 1 << '\n'; //2
+
+    int Array3[4] = { 100,200,300,400 };
+
+    for (int i = 0;i < 4;i++) {
+
+        std::cout << Array3[i] << '\n'; //100 200 300 400
+
+    }
+
+
+    char Array4[1][1]{ {'a'} };
+    std::cout << Array4[0][0] << '\n'; //a
+
+    char Array5[1][1][1]{ { {'b'} } };
+    std::cout << Array5[0][0][0] << '\n'; //b
+
+
+    char Array6[3][2]{ {'a','l'},{'p','e'},{'r','e'}};
+    std::cout << Array6[2][1] << '\n'; //e
+
+    char Array7[4][2]{ {'a','l'},{'p','e'},{'r','e'},{'n'}};
+
+    for (int i = 0;i < 4;i++) {
+        for (int i2 = 0;i2 < 2;i2++) {
+            std::cout << Array7[i][i2] << '\n'; // a l p e r e n
+        }
+    }
+    const int number = 10; 
+    int Array8[number] = { 0,1,2,3,4,5,6,7,8,9 }; //static array
+    int arraylenght = std::size(Array8);
+
+    std::cout << arraylenght << '\n'; // 10
+
+    int Array9[2] = { 1,2 };
+
+    func1(Array9);
 }
 
-// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
-// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+void func1(int Array [2]) {
 
-// Kullanmaya Başlama İpuçları: 
-//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
-//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
-//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
-//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
-//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
-//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
+ 
+    std::cout << Array << '\n'; // Adress
+    for (int i = 0;i < 2;i++) {
+
+        std::cout << Array[i] << '\n'; // 1 2 
+
+
+
+    }
+
+}
