41f73b4db23d7e65ee396cc4c8ad33d4a00fca3e
alperenbugaz <alperenbugaz@gmail.com>
2024-08-08 18:32:06 +0300

    İstisna Durumlar (Try-Catch) Eklendi

--- 8-Nesneye Yönelik Programlama/8-Nesneye Yönelik Programlama.cpp
+++ 8-Nesneye Yönelik Programlama/8-Nesneye Yönelik Programlama.cpp
@@ -6,7 +6,7 @@ public:
     int b;
     int result = 0;
     void topla() { this->result = this->a + this->b; }  //Metot
-    void print() { std::cout << this->result<<'\n'; }
+    void print() { std::cout << this->result << '\n'; }
 
     int width;
     int height;
@@ -21,19 +21,17 @@ class Payment {
 public: //her yerden erişilebilir
     std::string parabirimi = "TL";
     std::string Tutar = "0.0";
-    
-        std::cout << "Veriler bankaya gönderiliyor. Parabirimi:" + this->parabirimi + "Tutar:" + this->Tutar << '\n'; 
+
+    void sorgula() {
+
+        std::cout << "Veriler bankaya gönderiliyor. Parabirimi:" + this->parabirimi + "Tutar:" + this->Tutar << '\n';
     }
 
-private: //hiçbir yerden erişilemez
-    std::string payment_id = "123456"; 
+private: //hiçbir yerden erişilemez //default
+    std::string payment_id = "123456";
 
 
 protected: //Miras alan bir class erişebilir
-    void sorgula() {
-
-        std::cout << "Payment ID numarası:" + this->payment_id << '\n';
-    }
 
     std::string msg = "";
 
@@ -41,17 +39,200 @@ protected: //Miras alan bir class erişebilir
 
 
 class Test :public Payment {
-    
-    protected:
-        void changeparametre() { 
-            Payment::msg = "Alperen"; 
-            Payment::parabirimi = "Euro";
-            Payment::Tutar = "100";
-            //Payment::payment_id 'ye private olduğu için erişilemez
-        };
+
+protected:
+    void changeparametre() {
+        Payment::msg = "Alperen";
+        Payment::parabirimi = "Euro";
+        Payment::Tutar = "100";
+        //Payment::payment_id 'ye private olduğu için erişilemez
+    };
 
 };
 
+
+class Country { //base class
+public:
+    int population;
+    std::string capitalcity;
+
+    Country() {
+        std::cout << "Start Country" << '\n';
+    }
+
+    ~Country() {
+        std::cout << "Stop Country" << '\n';
+    }
+}; //soyut sınıf
+
+class Turkiye : public Country { //derived class 
+public:
+   
+   
+    
+    void info() {
+    
+            
+        int pop = this->population;
+        std::string capital = this->capitalcity;
+        std::cout << "Capital City:" ;
+        std::cout << capital << '\n';
+
+
+    }
+    Turkiye() {
+        std::cout << "Start Turkiye "  << '\n';
+
+    }
+    ~Turkiye() {
+        std::cout << "Stop Turkiye" << '\n';
+    }
+};
+
+class Germany : public Country { //derived class 
+public:
+    Germany() {
+        std::cout << "Start Germany " << '\n';
+
+    }
+    ~Germany() {
+        std::cout << "Stop Germany" << '\n';
+    }
+};
+
+class UnsignedNumber {
+
+private:
+    int number;
+
+public:
+    void controlnumber(int t) {
+
+        if (t > 0) {
+
+            number = t;
+        }
+
+        else {
+            number = -1 * t;
+        }
+        shownumber();
+
+    }
+    void shownumber() {
+
+        std::cout << number << '\n';
+
+    }
+
+
+
+};
+
+class Car {
+public:
+    int speed;
+    int color;
+    std::string country;
+    virtual void showcolor() =0 ; //sanal metot
+};
+
+class Mercedes : public Car {
+
+    void showcolor() {
+        std::cout << "Siyah" << '\n';
+
+    }
+
+};
+
+class BMW : public Car {
+
+    void showcolor() {
+        std::cout <<"Beyaz" << '\n';
+
+    }
+
+};
+
+class Ferrari : public Car {
+
+    void showcolor() {
+        std::cout << "Kırmızı" << '\n';
+
+    }
+};
+
+
+class Animals { //Abstract 
+
+public:
+    int a; //soyut sınıflarda değişken bulunabilir. Değişken bulunmazsa Interface olur.
+    int b;
+    virtual void Sound() = 0; //Soyut sınıflarda gövdesiz metotlar bulunur
+
+};
+
+class Dog: public Animals{
+   
+public:
+    void Sound() {
+
+        std::cout << "Hav";
+    }
+
+};
+
+class Cat :public Animals {
+public:
+    void Sound() {
+
+        std::cout << "Miyav";
+    }
+
+};
+
+class Duck :public Animals {
+public:
+    void Sound() {
+
+        std::cout << "Vak";
+    }
+
+};
+
+void AnimalsSound(Animals* Animalsptr) {
+
+    Animalsptr->Sound();
+
+}
+
+
+void showcarcolors(Car* CarPtr) {
+
+    CarPtr->showcolor();
+
+}
+
+void counter() {
+    static int a = 0; //bu değişken statik olduğu için hep adreste tutulur. Atama işlemi yalnızca fonksiyon ilk çalıştığında gerçekleşir.
+    a++;
+    std::cout <<'\n';
+
+    std::cout << a << '\n';
+}
+
+
+class Book {
+
+public:
+    static int pieces ; //statik değişkenlerin atamaları class içinde yapılmamalı
+    Book() {
+
+        pieces++;
+    }
+};
+int Book::pieces = 0; //class içinde atama yapılamaz
 
 
 int main()
@@ -69,6 +250,70 @@ int main()
 
     Payment pay;
 
+    Turkiye TR;
+    TR.population = 80;
+    TR.capitalcity = "Ankara";
+    std::cout << TR.population << '\n'; //80
+    
+    TR.info(); //Ankara
+
+    Turkiye* TRPtr = &TR;
+    std::cout << TRPtr << '\n'; //ADRESS
+    
+
+    TRPtr->population = 100;
+    std::cout << "TR Population:"; 
+
+    std::cout <<  TR.population << '\n'; //100
+
+    //reference
+    int number1 = 10;
+    int& refnumber1 = number1;
+
+    refnumber1 = 20;
+
+    std::cout <<  number1 << '\n';; //20 referans alan değişken değişirse normal değişken de değişir
+
+    number1 = 30;
+    std::cout << refnumber1 << '\n';; //30
+
+
+    //Kapsülleme
+    UnsignedNumber UN;
+    UN.controlnumber(5); //5
+    UN.controlnumber(-5); //5
+
+    Mercedes MercedesCar;
+    Ferrari FerrariCar;
+    BMW BMWCar;
+
+    showcarcolors(&MercedesCar); //siyah
+    showcarcolors(&FerrariCar); //kırmızı
+    showcarcolors(&BMWCar); //beyaz
+
+    //Animals animal; //soyut sınıflardan nesne türetilemez başka sınıfları yönetmek için kullanılır.
+
+    Cat cat;
+    Dog dog;
+
+    AnimalsSound(&cat); //miyav
+    AnimalsSound(&dog); //hav
+
+    counter(); //1
+    counter(); //2
+    counter(); //3
+    
+    
+    Book book;
+    Book book2;
+    
+    //book.pieces = 10;
+    //std::cout << book.pieces << '\n';; //10
+    //book2.pieces = 20;
+    //std::cout << book2.pieces << '\n';; //20
+    //std::cout << book.pieces << '\n';; //20
+
+    std::cout << Book::pieces; //2
+
 
 }
-
---
+++ 9-İstisnalar/9-İstisnalar.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+class ErrorManagemnt {
+private:
+    std::string message;
+
+public:
+    ErrorManagemnt(std::string message) {
+        this->message = message;
+
+    }
+
+    std::string yaz() {
+        
+        return this->message;
+    }
+};
+
+int division(int a, int b) {
+
+    try {
+        if (b == 0) {
+            throw  ErrorManagemnt("Math Error"); //throw,hata durumunda "Math error" stringini catche gönderir
+
+        }
+        else if (a > b) {
+            std::cout << a / b;
+            return a/b;
+        }
+
+    }
+    catch (ErrorManagemnt Error) {
+        std::cout << Error.yaz();
+    }
+}
+
+int main()
+{
+    int a = 10;
+    int b = 1;
+    int c = 0;
+    division(a, b); //10
+    division(a, c); //"Math Error" mesajı
+}
