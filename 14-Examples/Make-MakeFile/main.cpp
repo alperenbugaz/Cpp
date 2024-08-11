681c79391fecfa66a31251e1f744a3be51aeae86
alperenbugaz <alperenbugaz@gmail.com>
2024-08-11 04:32:52 +0300

    Makefile Eklendi

---
+++ 14-Examples/Make-MakeFile/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int topla (int a,int b);
+int main (){
+
+std::cout << "Hello World!!" << std::endl;
+std::cout <<"\n";
+
+
+std::cout <<topla(10,20);
+
+
+int  a;
+std::cin >>a;
+std::cout << a;
+
+}
