812940bac0649ea98969025f78b8ca8677e4a2ac
alperenbugaz <alperenbugaz@gmail.com>
2024-08-13 00:42:31 +0300

    CMake Eklendi

---
+++ 14-Examples/CMake/build/CMakeFiles/ProjectName.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectName.dir/main.cpp.obj"
+  "CMakeFiles/ProjectName.dir/main.cpp.obj.d"
+  "libProjectName.dll"
+  "libProjectName.dll.a"
+  "libProjectName.dll.manifest"
+  "libProjectName.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjectName.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 14-Examples/CMake/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Cmake Build" << endl;
+    int a;
+    cin >> a;
+    return 0;
+}
